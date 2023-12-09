14b5d013e1c6ac9127bd7ec35828adeb31aab6e9
Aleksey Khnyrev <alekseykhnyrev@Alekseys-MacBook-Pro.local>
2023-12-09 22:59:52 +0400

    first try appium test

---
+++ firsr_test.py
@@ -0,0 +1,27 @@
+import pytest
+from appium import webdriver
+
+
+@pytest.fixture(scope="class")
+def set_up(request):
+    desired_caps = {
+        "platformName": "Android",
+        "deviceName": "emulator-5554",
+        "platformVersion": "8.0.0",
+        "appPackage": "org.wikipedia",
+        "appActivity": ".main.MainActivity",
+        "automationName": "Appium",
+        "app": "/Users/alekseykhnyrev/PycharmProjects/PythonAppiumProject/PythonAppiumProject/apks/org.wikipedia.apk"
+    }
+    # Подключение к Appium серверу
+    driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+
+    def _tear_down():
+        driver.quit()
+
+    request.addfinalizer(_tear_down)
+    return driver
+
+
+def test_first():
+    print("FIRST TEST RUUUUN!")
