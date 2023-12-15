d6ca3448ba44ee322959450081554614d07e3649
Aleksey Khnyrev <alekseykhnyrev@Alekseys-MacBook-Pro.local>
2023-12-15 20:45:18 +0400

    moved the functions to the utils.py

--- test_first.py
+++ test_first.py
@@ -1,14 +1,12 @@
 import pytest
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
-from appium.webdriver.appium_service import AppiumService
 from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from .utils import wait_for_element, wait_for_element_and_click, wait_for_element_and_send_keys
 
-APPIUM_PORT = 4723
-APPIUM_HOST = '127.0.0.1'
-TIMEOUT = 5
+# APPIUM_PORT = 4723
+# APPIUM_HOST = '127.0.0.1'
+# TIMEOUT = 5
 
 
 @pytest.fixture
@@ -31,18 +29,6 @@ def get_driver(request):
 
 def test_first(get_driver):
     import time
-
-    def wait_for_element(driver, locator, timeout=20, error_message="текст по умолчанию"):
-        wait_result = WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator), error_message)
-        return wait_result
-
-    def wait_for_element_and_click(driver, locator, timeout=20, error_message="текст по умолчанию"):
-        wait_result = WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator), error_message)
-        return wait_result.click()
-
-    def wait_for_element_and_send_keys(driver, locator, string_value, timeout=20, error_message="текст по умолчанию"):
-        wait_result = WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator), error_message)
-        return wait_result.send_keys(string_value)
 
     main_page_search_field_locator = (AppiumBy.ID, "search_container")
     wait_for_element_and_click(get_driver, main_page_search_field_locator, 10)
---
+++ utils.py
@@ -0,0 +1,18 @@
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def wait_for_element(driver, locator, timeout=20, error_message="текст по умолчанию"):
+    wait_result = WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator), error_message)
+    return wait_result
+
+
+def wait_for_element_and_click(driver, locator, timeout=20, error_message="текст по умолчанию"):
+    wait_result = WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator), error_message)
+    return wait_result.click()
+
+
+def wait_for_element_and_send_keys(driver, locator, string_value, timeout=20, error_message="текст по умолчанию"):
+    wait_result = WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator), error_message)
+    return wait_result.send_keys(string_value)
